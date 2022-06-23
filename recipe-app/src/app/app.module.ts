e0ff96a0efe92d68ae0a8f85e32698d7000ee345
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-06-23 11:00:37 +0300

    Create components

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
