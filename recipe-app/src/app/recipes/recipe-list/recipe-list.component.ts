eef9e32198a183a4968788ecbfe41694050a262d
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-06-24 20:59:32 +0300

    Create recipe model and array and display it

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Lasagne', 'A traditional Italian pasta dish.', 'https://images.ctfassets.net/0yf82hjfqumz/56IspPW5UKTP9xapSKKHhM/e63caf87f113642734445ec6f4325e8c/Juustoinen-lasagne.jpg?fit=fill&h=900&q=60&w=1600')
+  ];
   constructor() { }
 
   ngOnInit(): void {
